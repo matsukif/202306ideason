a406c974a3c4392342b5bd77ed0e281de36c6a63
matsukif <100049736+matsukif@users.noreply.github.com>
2023-07-02 09:30:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,15 @@ system_prompt = """
 """
 
 
-# st.session_stateを使いメッセージのやりとりを保存
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "system", "content": system_prompt}
-        ]
+# # st.session_stateを使いメッセージのやりとりを保存
+# if "messages" not in st.session_state:
+#     st.session_state["messages"] = [
+#         {"role": "system", "content": system_prompt}
+#         ]
+
+st.session_state["messages"] = [
+    {"role": "system", "content": system_prompt}
+    ]
 
 # チャットボットとやりとりする関数
 def communicate():
